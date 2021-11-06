987b453d040d85e84796db7f0cce82ffe3b06e6e
Krzakooo <55135961+Krzakooo@users.noreply.github.com>
2021-11-06 13:20:11 +0100

    Add project files.

---
+++ maturka52/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace maturka52
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
